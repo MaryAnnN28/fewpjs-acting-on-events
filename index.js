445378a548f18a8a1fc96bf9b870a4e39e12f6c0
MaryAnnN28 <maryann.navarrete@gmail.com>
2021-01-12 23:36:53 -0500

    done

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Your code here
+dodger = document.getElementById('dodger'); 
+
+dodger.style.backgroundColor = "#FF69B4"; 
+
+const findLeftNumber = function(element) {
+   return parseInt(element.style.left.replace('px', ''), 10);
+}
+
+function moveDodgerLeft() {
+   let left = findLeftNumber(dodger)
+   if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+   };
+}
+
+function moveDodgerRight() {
+   let left = findLeftNumber(dodger)
+   if (left < 360) {
+      dodger.style.left = `${left + 1}px`;
+   };
+}
+
+document.addEventListener("keydown", function(e) {
+   if (e.key === "ArrowLeft") {
+      moveDodgerLeft();
+   } else if (e.key === "ArrowRight") {
+      moveDodgerRight();
+   }
+});
